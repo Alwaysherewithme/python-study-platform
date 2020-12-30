c2376de042597bf2489e87ec657d53b8a4e1bccd
ZhouWenming <zhouwenming0401@qq.com>
2020-12-30 22:45:17 +0800

    build client

---
+++ build/precache-manifest.420da66e5478fda6b72ec9a1f3421c3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b43b0f1905723eebd01",
+    "url": "./static/css/main.5e784957.chunk.css"
+  },
+  {
+    "revision": "4b43b0f1905723eebd01",
+    "url": "./static/js/main.4b43b0f1.chunk.js"
+  },
+  {
+    "revision": "2eb3be7f9efd984b7a79",
+    "url": "./static/css/1.8a0c2d3c.chunk.css"
+  },
+  {
+    "revision": "2eb3be7f9efd984b7a79",
+    "url": "./static/js/1.2eb3be7f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "fc90cabc4e2d5b49fcd7c544699f9dd3",
+    "url": "./index.html"
+  }
+];
